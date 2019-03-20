4de114d9741162d180d0512ce131748d6da236d1
Manav Bokinala <manav.bokinala@gmail.com>
2019-03-19 17:28:59 -0700

    bug fixes

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ $('#mainForm').submit(function (e) {
     e.preventDefault();
     console.log($('#longURL').val());
 
+    var sendUrl = $('#longURL').val();
+    if(!(sendUrl.startsWith('http://') || sendUrl.startsWith('https://'))) {
+        sendUrl = 'http://' + sendUrl;
+    }
+
     data = {
         url: $('#longURL').val(),
         code: $('#code').val()
